6dbc2b5b51b9d05e49955798f887902740940921
Hyun Sim <skhy1808@hotmail.com>
2025-04-03 19:27:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     author="hyun wook sim",  
     author_email="jay.sim@mail.utoronto.ca",  
-    url="https://github.com/yourusername/BayesianHorseshoe",  
+    url="https://github.com/byunbeee/BayesianHorseshoe",  
     packages=find_packages(),
     install_requires=[
         "numpy>=1.18.0",
